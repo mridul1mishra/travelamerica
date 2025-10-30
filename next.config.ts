48a66f1d1cb7836acabc21c37bfd4e57ddc1cc33
Divyesh Mishra <mridul1mishra@yahoo.com>
2025-10-29 21:28:11 -0400

    This is the csp fix

--- next.config.ts
+++ next.config.ts
@@ -10,13 +10,16 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-inline' https://www.googletagmanager.com;
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' data: https:;
-              font-src 'self' https://fonts.gstatic.com;
-              connect-src 'self' https://www.googletagmanager.com https://www.google-analytics.com;
-              frame-src 'none';
-            `.replace(/\s{2,}/g, " ").trim(), // compact the header
+      script-src 'self'
+        https://www.googletagmanager.com
+        https://www.google-analytics.com
+        'sha256-dj8U3DHIoYgqto74jrgiN/gt4JUGBkkmPMdLxxCuZ6c=';
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' data: https:;
+      font-src 'self' https://fonts.gstatic.com;
+      connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com;
+      frame-src 'none';
+    `.replace(/\s{2,}/g, " ").trim(),
           },
           
         ],
