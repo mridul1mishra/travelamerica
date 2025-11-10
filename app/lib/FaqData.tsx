95823b1bf8d68c7e02155d132e7b3009f0ae5493
Divyesh Mishra <mridul1mishra@yahoo.com>
2025-11-10 17:41:05 -0500

    JSON ERROR

--- app/lib/FaqData.tsx
+++ app/lib/FaqData.tsx
@@ -1,6 +1,6 @@
 export async function getFaq() {
   const [faqTravelHubData] = await Promise.all([
-    fetch(`https://www.travelamerica.work/data//faq/travelhubfaq.json`).then(res => res.json()),
+    fetch(`/data//faq/travelhubfaq.json`).then(res => res.json()),
   ]);
   return {faqTravelHubData };
 }
