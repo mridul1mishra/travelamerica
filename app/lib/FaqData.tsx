0e56fdd9c32ad239c416a191be0a305a9fd03995
Divyesh Mishra <mridul1mishra@yahoo.com>
2025-11-10 17:36:05 -0500

    build error for localhost url

--- app/lib/FaqData.tsx
+++ app/lib/FaqData.tsx
@@ -1,6 +1,6 @@
 export async function getFaq() {
   const [faqTravelHubData] = await Promise.all([
-    fetch(`http://localhost:3000/data//faq/travelhubfaq.json`).then(res => res.json()),
+    fetch(`https://www.travelamerica.work/data//faq/travelhubfaq.json`).then(res => res.json()),
   ]);
   return {faqTravelHubData };
 }
