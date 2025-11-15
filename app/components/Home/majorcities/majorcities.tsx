e60f4f146783531c12c564c5a6362da8e1427dff
unknown <mridul1mishra@yahoo.com>
2025-11-15 01:40:52 +0000

    seo fix

--- app/components/Home/majorcities/majorcities.tsx
+++ app/components/Home/majorcities/majorcities.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./majorcities.css"; // optional if you want to style it
 import { StoriesSectionProps } from "../../../models/story";
 import StoryCard from "./storycard/storycard";
+import Head from "next/head";
 
 interface MajorCitiesProps {
   section: StoriesSectionProps;
@@ -11,6 +12,21 @@ interface MajorCitiesProps {
 export default function Majorcities({ section: { heading, storydescription, stories } }: StoriesSectionProps) {
   
     return (
+      <>
+      <Head>
+        <link
+          rel="preload"
+          as="font"
+          href="/data/corben-cdnfonts/Corben-Bold.ttf"
+          type="font/woff2"
+        />
+        <link
+          rel="preload"
+          as="image"
+          href="/data/logo3.png"
+          type="image/png"
+        />
+      </Head>
         <section className="travel-stories">
   <button onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
   className="back-to-top"><h2  className="section-heading">{heading}</h2></button>
@@ -26,5 +42,6 @@ export default function Majorcities({ section: { heading, storydescription, stor
     <a href="/majorcities/newyork" className="view-more-btn">View More</a>
   </div>
         </section>
+        </>
     );
 };
