ece0ab099660783030b0422460b736154acd7999
unknown <mridul1mishra@yahoo.com>
2025-11-20 02:23:01 +0000

    solo travel fix

--- app/majorcities/[city]/solo-travel/solotravelclient.tsx
+++ app/majorcities/[city]/solo-travel/solotravelclient.tsx
@@ -80,9 +80,10 @@ export default function SoloTravelPage() {
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schema2) }}/>
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schema1) }}/>
         <Header image={`/data/majorcities/${city}/assets/safetybanner.png`} bannerText={city && !citiesWithoutBanner.includes(city) ? (<span style={{ color: '#1A2A3A', fontWeight: '600', fontSize: '40px' }}>Solo Travel in NYC: Safe Itinerary, <br />Packing Tips and Best Landmarks!</span>) : ""} />
-        <section className={styles.splitSection} style={{ paddingTop: "175px", textAlign: "center" }}>
+        <section className={styles.splitSection} style={{ paddingTop: "105px", textAlign: "center" }}>
                 <div style={{ width: "100%", textAlign: "center" }}>
-                <h1  className={styles.sectionheading}>Solo Travel in NYC: Safe Itinerary,<br />Packing Tips and Best Landmarks!</h1>
+                <h1  className={styles.sectionheading}>Solo Travel in NYC!</h1>
+                <p>Safe Itinerary, Packing Tips and Best Landmarks — Imagine stepping off the subway alone at midnight, with Broadway lights guiding your way. NYC offers solo travelers both excitement and safety — if you know where to go.</p>
                 </div>
         </section> 
         <section className={styles.splitSection}>
