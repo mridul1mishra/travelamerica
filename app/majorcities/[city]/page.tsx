0934ea88bbe9a696c9085323bbafa4c6335b3449
unknown <mridul1mishra@gmail.com>
2025-11-30 21:50:08 +0000

    schema json

--- app/majorcities/[city]/page.tsx
+++ app/majorcities/[city]/page.tsx
@@ -113,7 +113,7 @@ export default async function CityPage({ params }: PageProps) {
               
               <Placevisit city={city}/>
               <Footer />
-              
+              <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schema1) }}/>
       </div>
       </>
     );
