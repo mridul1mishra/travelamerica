5ef15beba2a3a6798a8d0c6fd8ced7cc556b88d4
unknown <mridul1mishra@gmail.com>
2025-12-02 23:41:48 +0000

    title file food page

--- app/majorcities/newyork/food/page.tsx
+++ app/majorcities/newyork/food/page.tsx
@@ -1,5 +1,5 @@
 export const generateMetadata = () => ({
-  title: 'Discover the Best Solo Dining Spots and Flavorful Food Halls in NYC',
+  title: 'Solo Travel NYC: Best Dining Spots & Food Halls for Exploring Alone',
   description:  'Plan your solo NYC adventure with tips on safety, packExplore the best solo dining spots in NYC, from cozy counters to vibrant food halls. Savor iconic bagels, ramen, and more for the perfect solo meal!',
   robots: {
     index: true,
