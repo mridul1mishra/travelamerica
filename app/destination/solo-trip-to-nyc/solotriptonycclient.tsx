a7808cf785639a2cc13aead8129eabca8b7811fc
unknown <mridul1mishra@gmail.com>
2025-12-22 14:05:30 +0000

    pillar page

---
+++ app/destination/solo-trip-to-nyc/solotriptonycclient.tsx
@@ -0,0 +1,62 @@
+"use client";
+import styles from './solotriptonyc.module.css';
+import { useState } from "react";
+import Image from "next/image";
+
+export default function NYCsafetyforsolofemaletravelerclient() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => setIsMenuOpen((prev) => !prev);
+  const closeMenu = () => setIsMenuOpen(false);
+    return(
+        <>
+        <section className={`${styles.overlayheader} ${styles.scrolled}`}>
+        <div className={styles.brandlogo}>          
+            <a href="/">                   
+                <Image src="/data/logo.webp" alt="Travel Americas Logo" width={150} height={150}  sizes="(max-width: 768px) 100px, 150px" fetchPriority="high"/>
+            </a>
+        </div>
+        <nav className={styles.mainnav}>
+          <button className={styles.hamburger} onClick={toggleMenu} aria-label="Toggle menu">
+     ☰
+          </button>
+          <ul className={`${styles.navlinks} ${isMenuOpen ? styles.active : ""}`}>
+            <li className={styles.closebtn}>
+              <button onClick={closeMenu} aria-label="Close menu">✕</button>
+            </li>
+           <li style={{ cursor: "pointer" }}><a href="/majorcities/newyork">New York</a></li>
+           <li style={{ cursor: "pointer" }}><a href="/majorcities/losangeles">Los Angeles</a></li>
+           <li style={{ cursor: "pointer" }}><a href="/majorcities/lasvegas">Las Vegas</a></li>
+           <li style={{ cursor: "pointer" }}><a href="/majorcities/orlando">Orlando</a></li> 
+          </ul>
+        </nav>
+        </section>
+        <section className={styles.hero}>
+            <div className={styles.herocontainer}>            
+                <div className={styles.herotext}>
+                    <h1>NYC Solo Travel Guide</h1>
+                    <p className={styles.subtext}>Safe, confident, and stress‑free solo travel in NYC.</p>
+
+                    <div className={styles.herobuttons}>
+                        <a href="#" className={`${styles.btn} ${styles.primary}`}>Start with Safety </a>
+                        <a href="#" className={`${styles.btn} ${styles.secondary}`}>Where to Stay</a>
+                    </div>
+
+                    <p className={styles.updated}>Updated for 2025</p>
+                </div>
+
+                
+                <div className={styles.heroImage}>
+                <div className={styles.imagePlaceholder}>
+                    <div className={styles.heroImageWrapper}>
+                        <Image src="/destination/nyc-travel-editorial-collage-new.png" alt="Travel Americas Logo" fill style={{ objectFit: "contain" }} fetchPriority="high"/>
+                
+                    </div>
+                </div>
+                </div>
+
+            </div>
+        </section>
+
+        </>
+    );
+}
