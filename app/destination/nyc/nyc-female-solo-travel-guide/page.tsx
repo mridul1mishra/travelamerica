25ade28f22b25f3fe5ef5d9cb114a0a658c3804d
unknown <mridul1mishra@gmail.com>
2025-12-24 21:47:08 +0000

    female travel guide

---
+++ app/destination/nyc/nyc-female-solo-travel-guide/page.tsx
@@ -0,0 +1,15 @@
+
+import ClientPage from "./pageclient";
+
+export const metadata = {
+  title: "NYC Female Solo Travel Guide (2025)",
+  description:
+    "A practical, confidence-building guide for solo female travelers visiting New York City. Safety tips, best neighborhoods, subway advice, and more.",
+};
+
+export default function Page() 
+{ 
+    return ( 
+    <> <ClientPage /> </> 
+    ); 
+}
