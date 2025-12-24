39305775841f1b2cdcb204551e1ccd3cf5280169
mridul1mishra <45389580+mridul1mishra@users.noreply.github.com>
2025-12-23 22:38:36 -0500

    Update page.tsx

--- app/destination/nyc/best-areas-to-stay/page.tsx
+++ app/destination/nyc/best-areas-to-stay/page.tsx
@@ -1,8 +1,8 @@
 import Script from "next/script";
 
 export const generateMetadata = () => ({
-  title: 'Solo Trip to NYC (2025 Guide): Safety, Itinerary, Neighborhoods & Tips',
-  description:  'Solo Trip to NYC (2025 Guide): Safety, Itinerary, Neighborhoods & Tips',
+  title: 'Best Areas to Stay in NYC 2026: Safest & Most Convenient',
+  description:  'Find the best areas to stay in NYC for safety, convenience, and first‑time visitors. Compare top neighborhoods with pros, cons, and transit tips.',
   robots: {
     index: true,
     follow: true,
@@ -12,8 +12,8 @@ export const generateMetadata = () => ({
     },
     twitter: {
     card: "summary_large_image",
-    title: 'Solo Trip to NYC (2025 Guide): Safety, Itinerary, Neighborhoods & Tips',
-    description:  'Plan your NYC solo trip with safe areas, smart transit tips, and a simple 3‑day itinerary.',
+    title: 'Best Areas to Stay in NYC 2026: Safest & Most Convenient',
+    description:  'Find the best areas to stay in NYC for safety, convenience, and first‑time visitors. Compare top neighborhoods with pros, cons, and transit tips.',
     images: ["https://www.travelamerica.work/social/solo-trip-to-nyc-gritty.png"]
   }
 });
