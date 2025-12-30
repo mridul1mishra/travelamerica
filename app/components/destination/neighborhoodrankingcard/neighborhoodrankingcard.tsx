dc6759636223d09730419f4fe2f9396af94520aa
mridul1mishra <45389580+mridul1mishra@users.noreply.github.com>
2025-12-30 18:08:57 -0500

    Update neighborhoodrankingcard.tsx

--- app/components/destination/neighborhoodrankingcard/neighborhoodrankingcard.tsx
+++ app/components/destination/neighborhoodrankingcard/neighborhoodrankingcard.tsx
@@ -60,7 +60,7 @@ export default function NeighborhoodRankingCards() {
  ];
 
   return (
-    <div class="container">
+    <div className={styles.container}>
     <section id="neighborhood" className={styles.wrapper}> 
         <h2>Best Neighborhoods to Stay in NYC (Ranked for Solo Travelers)</h2> 
         <p className={styles.intro}> These neighborhoods offer the best mix of safety, walkability, transit access, and comfort for solo travelers. Each one serves a different travel style — choose the vibe that fits you. </p> 
