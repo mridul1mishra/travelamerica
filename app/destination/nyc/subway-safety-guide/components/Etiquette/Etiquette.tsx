ac1656c0dcfcb6e433cb2b741b51ce1296daf720
mridul1mishra <45389580+mridul1mishra@users.noreply.github.com>
2026-01-03 21:02:09 -0500

    Update Etiquette.tsx

--- app/destination/nyc/subway-safety-guide/components/Etiquette/Etiquette.tsx
+++ app/destination/nyc/subway-safety-guide/components/Etiquette/Etiquette.tsx
@@ -1,5 +1,6 @@
 import styles from "./Etiquette.module.css";
 import type { Etiquette as EtiquetteType } from "../types-subwaySafetyGuide";
+import { FaSubway, FaUserFriends, FaClock, FaEyeSlash } from "react-icons/fa";
 
 interface Props {
   data: EtiquetteType;
@@ -20,7 +21,7 @@ export default function Etiquette({ data }: Props) {
 
   <div className={styles.grid}>
     <div className={styles.item}>
-      <h3 className={styles.heading}>Platform Etiquette</h3>
+      <h3 className={styles.heading}><FaSubway className={styles.icon} />Platform Etiquette</h3>
       <ul className={styles.list}>
         {platformEtiquetteList.map((item, index) => (
           <li key={index} className={styles.text}>{item}</li>
@@ -29,7 +30,7 @@ export default function Etiquette({ data }: Props) {
     </div>
 
     <div className={styles.item}>
-      <h3 className={styles.heading}>Train Etiquette</h3>
+      <h3 className={styles.heading}><FaUserFriends className={styles.icon} />Train Etiquette</h3>
       <ul className={styles.list}>
         {trainEtiquetteList.map((item, index) => (
           <li key={index} className={styles.text}>{item}</li>
@@ -38,7 +39,7 @@ export default function Etiquette({ data }: Props) {
     </div>
 
     <div className={styles.item}>
-      <h3 className={styles.heading}>Rush Hour Rules</h3>
+      <h3 className={styles.heading}><FaClock className={styles.icon} />Rush Hour Rules</h3>
       <ul className={styles.list}>
         {rushHourRulesList.map((item, index) => (
           <li key={index} className={styles.text}>{item}</li>
@@ -47,7 +48,7 @@ export default function Etiquette({ data }: Props) {
     </div>
 
     <div className={styles.item}>
-      <h3 className={styles.heading}>Unspoken Norms</h3>
+      <h3 className={styles.heading}><FaEyeSlash className={styles.icon} />Unspoken Norms</h3>
       <ul className={styles.list}>
         {unspokenNormsList.map((item, index) => (
           <li key={index} className={styles.text}>{item}</li>
