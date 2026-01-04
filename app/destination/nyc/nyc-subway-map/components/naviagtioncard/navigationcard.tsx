67f3913187828a908ac20db2afd68e902f65bca8
mridul1mishra <45389580+mridul1mishra@users.noreply.github.com>
2026-01-04 18:11:32 -0500

    Update navigationcard.tsx

--- app/destination/nyc/nyc-subway-map/components/naviagtioncard/navigationcard.tsx
+++ app/destination/nyc/nyc-subway-map/components/naviagtioncard/navigationcard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import styles from "./navigationcard.module.css";
-import type { QuickNavItem } from "./nyc-subway-map";
+import type { QuickNavItem } from "../../nyc-subway-map";
 
 interface IconCardGridProps {
   items: QuickNavItem[];
@@ -28,5 +28,6 @@ export default function IconCardGrid({ items }: IconCardGridProps) {
     </section>
   );
 }
+
 
 
