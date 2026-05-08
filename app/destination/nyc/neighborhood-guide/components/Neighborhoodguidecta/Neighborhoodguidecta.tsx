1cd35f36ff96cdcfef901fdcbe94ad26c3359d10
mridul mishra <mridul1mishra@yahoo.com>
2026-05-08 18:00:26 -0400

    Neighborhood guid fix for the code

--- app/destination/nyc/neighborhood-guide/components/Neighborhoodguidecta/Neighborhoodguidecta.tsx
+++ app/destination/nyc/neighborhood-guide/components/Neighborhoodguidecta/Neighborhoodguidecta.tsx
@@ -2,10 +2,11 @@ import Link from 'next/link';
 import styles from './Neighborhood.module.css';
 
 type NeighborhoodGuideCTAProps = {
+  tripType?: string;
   interest?: string;
 }
 
-export default function NeighborhoodGuideCTA({interest}: NeighborhoodGuideCTAProps) {
+export default function NeighborhoodGuideCTA({tripType, interest}: NeighborhoodGuideCTAProps) {
   return (
     <section className={styles.section}>
       <h2 className={styles.heading}>
